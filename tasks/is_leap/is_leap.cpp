d3bfd4b378a18f6a7b9435a6eec1a8976924bedd
Nikita Oleynik <naoleynik@edu.hse.ru>
2023-04-14 01:03:33 +0300

    commit 1

--- tasks/is_leap/is_leap.cpp
+++ tasks/is_leap/is_leap.cpp
@@ -1,5 +1,10 @@
 #include "is_leap.h"
 
 bool IsLeap(int year) {
-    return true;
+    const int one = 100;
+    const int four = 400;
+    if ((year % 4 == 0 && year % one != 0) || (year % four == 0)) {
+        return true;
+    }
+    return false;
 }

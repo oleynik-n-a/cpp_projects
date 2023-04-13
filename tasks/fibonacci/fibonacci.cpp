44bde27ecac4c40b7cf9d3af5928c13dd98ca341
Oleynik Nikita <naoleynik@edu.hse.ru>
2023-04-13 18:21:04 +0300

    commit 1

--- tasks/fibonacci/fibonacci.cpp
+++ tasks/fibonacci/fibonacci.cpp
@@ -1,5 +1,12 @@
 #include "fibonacci.h"
 
 int64_t CalculateNthFibonacci(int64_t n) {
-    return 0;
+    int64_t prev = 0;
+    int64_t curr = 1;
+    for (int64_t i = 0; i < n; ++i) {
+        int64_t temp = prev + curr;
+        prev = curr;
+        curr = temp;
+    }
+    return prev;
 }

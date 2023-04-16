f35007f280b38987a3542581d9d62f6097fe1448
Nikita Oleynik <naoleynik@edu.hse.ru>
2023-04-16 12:35:12 +0300

    commit 6

--- tasks/sum_digits/sum_digits.cpp
+++ tasks/sum_digits/sum_digits.cpp
@@ -2,11 +2,11 @@
 
 
 int64_t SumOfDigits(int64_t number) {
-	int64_t ans = 0;
-	while (number > 0) {
-		const int div = 10;
-		ans += number % div;
-		number /= div;
-	}
-	return static_cast<int64_t>(ans);
+    int64_t ans = 0;
+    while (number > 0) {
+        const int div = 10;
+        ans += number % div;
+        number /= div;
+    }
+    return static_cast<int64_t>(ans);
 }

4171a930752c8e0b31bcb1faf81df192ffbb70ed
Oleynik Nikita <naoleynik@edu.hse.ru>
2023-04-20 13:45:02 +0300

    commit 1

--- tasks/const_diagonal/const_diagonal.cpp
+++ tasks/const_diagonal/const_diagonal.cpp
@@ -1,4 +1,9 @@
 #include "const_diagonal.h"
 
 void BuildDiagonalArray(TableType table) {
+    for (int i = 0; i < LINES; i++) {
+        for (int j = 0; j < COLUMNS; j++) {
+            table[i][j] = (i + j + 1) * (i + j) / 2 + 2 * i - (i + j > LINES ? j : 0);
+        }
+    }
 }

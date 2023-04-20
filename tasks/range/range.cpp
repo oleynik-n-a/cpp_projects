09f2e12b6ad4954caddaad7f4ffcd0123f2a252f
Nikita Oleynik <naoleynik@edu.hse.ru>
2023-04-21 00:12:28 +0300

    range code style fix

--- tasks/range/range.cpp
+++ tasks/range/range.cpp
@@ -2,7 +2,7 @@
 
 std::vector<int> Range(int from, int to, int step) {
     std::vector<int> vector;
-    if (from == to || (from < to && (step <=0) ) || (from > to && step >= 0)) {
+    if (from == to || (from < to && (step <= 0)) || (from > to && step >= 0)) {
         return vector;
     }
     for (int i = from; from < to ? i < to : i > to; i += step) {

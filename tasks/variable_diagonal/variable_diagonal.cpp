0e835e285f95dd10f478498e737a04b3eb7c97ad
Nikita Oleynik <naoleynik@edu.hse.ru>
2023-04-20 22:44:57 +0300

    variable diagonal code style fix

--- tasks/variable_diagonal/variable_diagonal.cpp
+++ tasks/variable_diagonal/variable_diagonal.cpp
@@ -12,10 +12,10 @@ int** BuildDiagonalArray(size_t lines, size_t columns) {
     for (int i = 0; i < i_lines; ++i) {
         for (int j = 0; j < static_cast<int>(i_columns); ++j) {
             table[i][j] = i + j < i_lines
-                    ? (i + j) * (i + j + 1) / 2 + i
-                    : i_lines * i_columns -
-                      (i_lines + i_columns - (i + j) - 2) * (i_lines + i_columns - (i + j) - 1) /
-                      2 - i_lines + i;
+                              ? (i + j) * (i + j + 1) / 2 + i
+                              : i_lines * i_columns -
+                                    (i_lines + i_columns - (i + j) - 2) * (i_lines + i_columns - (i + j) - 1) / 2 -
+                                    i_lines + i;
         }
     }
     return table;

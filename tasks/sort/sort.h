9480763eb746ef49bbefdcca6f1a27e997eae615
Oleynik Nikita <naoleynik@edu.hse.ru>
2023-04-27 17:33:31 +0300

    sort commit 1

--- tasks/sort/sort.h
+++ tasks/sort/sort.h
@@ -2,4 +2,11 @@
 
 template <class Iterator, class Comparator = std::less<typename std::iterator_traits<Iterator>::value_type>>
 void Sort(Iterator begin, Iterator end, Comparator comparator = Comparator()) {
+    for (Iterator i = begin; i < end; ++i) {
+        for (Iterator j = i + 1; j < end; ++j) {
+            if (!comparator(*i, *j)) {
+                std::swap(*i, *j);
+            }
+        }
+    }
 }

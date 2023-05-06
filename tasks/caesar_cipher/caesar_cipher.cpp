3912304246e5fb7dd8df28f4cb319c1561445ced
Nikita Oleynik <naoleynik@edu.hse.ru>
2023-05-06 18:42:31 +0300

    caesar_cipher code style fix 2

--- tasks/caesar_cipher/caesar_cipher.cpp
+++ tasks/caesar_cipher/caesar_cipher.cpp
@@ -4,8 +4,8 @@ std::string Decipher(const std::string& cipher, size_t shift) {
     std::string decipher;
     for (int i = 0; i < static_cast<int>(cipher.length()); ++i) {
         decipher.push_back(cipher[i] - static_cast<int>(shift) >= 'A'
-                                  ? static_cast<char>(cipher[i] - static_cast<int>(shift))
-                                  : static_cast<char>('Z' - 'A' + cipher[i] + 1 - static_cast<int>(shift)));
+                                ? static_cast<char>(cipher[i] - static_cast<int>(shift))
+                                : static_cast<char>('Z' - 'A' + cipher[i] + 1 - static_cast<int>(shift)));
     }
     return decipher;
 }

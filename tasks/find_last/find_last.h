14f4ee9f5755dbe61f45a4156e6c85a456372b89
Nikita Oleynik <naoleynik@edu.hse.ru>
2023-05-06 13:43:05 +0300

    find_last UB fix

--- tasks/find_last/find_last.h
+++ tasks/find_last/find_last.h
@@ -2,10 +2,10 @@
 
 template <class Iterator, class T>
 Iterator FindLast(Iterator first, Iterator last, const T& val) {
-    for (Iterator i = last - 1; i >= first; --i) {
-        if (*i == val) {
+    for (Iterator i = last; i > first;) {
+        if (*--i == val) {
             return i;
         }
     }
-    return first;
+    return last;
 }

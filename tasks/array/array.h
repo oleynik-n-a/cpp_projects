3c7e3196f00ac8dcb70a31fb4cab6befd4e31cb1
Nikita Oleynik <naoleynik@edu.hse.ru>
2023-05-14 17:16:09 +0300

    array | commit 1

--- tasks/array/array.h
+++ tasks/array/array.h
@@ -46,3 +46,100 @@ public:
 
     Iterator end() const; // NOLINT
 };
+
+template <class T>
+Array<T>::Array() {
+    size_ = 0;
+    data_ = nullptr;
+}
+
+template <class T>
+Array<T>::Array(size_t size) {
+    size_ = size;
+    data_ = new T[size_];
+}
+
+template <class T>
+Array<T>::Array(std::initializer_list<T> list) {
+    size_ = list.size();
+    data_ = new T[size_];
+    size_t i = 0;
+    for (const auto& el : list) {
+        data_[i++] = el;
+    }
+}
+
+template <class T>
+Array<T>::Array(const Array& other) {
+    size_ = other.size();
+    data_ = new T[size_];
+    for (size_t i = 0; i < size_; ++i) {
+        data_[i] = other[i];
+    }
+}
+
+template <class T>
+Array<T>::~Array() {
+    if (data_) {
+        delete data_;
+    }
+}
+
+template <class T>
+Array<T>& Array<T>::operator=(const Array& other) {
+    size_ = other.size_;
+    data_ = new T[size_];
+    for (size_t i = 0; i < size_; ++i) {
+        data_[i] = other[i];
+    }
+    return *this;
+}
+
+template <class T>
+T& Array<T>::operator[](size_t index) {
+    return data_[index];
+}
+
+template <class T>
+const T& Array<T>::operator[](size_t index) const {
+    return data_[index];
+}
+
+template <class T>
+size_t Array<T>::size() const {
+    return size_;
+}
+
+template <class T>
+Array<T>::Iterator::Iterator(T* current) {
+    current_ = current;
+}
+
+template <class T>
+typename Array<T>::Iterator&
+Array<T>::Iterator::operator++() {
+    ++current_;
+    return *this;
+}
+
+template <class T>
+T& Array<T>::Iterator::operator*() {
+    return *current_;
+}
+
+template <class T>
+bool Array<T>::Iterator::operator!=(const Array::Iterator& other) {
+    return current_ != other.current_;
+}
+
+template <class T>
+typename Array<T>::Iterator
+Array<T>::begin() const {
+    return Iterator(data_);
+}
+
+template <class T>
+typename Array<T>::Iterator
+Array<T>::end() const {
+    return Iterator(data_ + size_);
+}

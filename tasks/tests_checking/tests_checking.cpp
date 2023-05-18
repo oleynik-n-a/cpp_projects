e3b18fbcc6129de1d3f49143b10a166f06facb98
Oleynik Nikita <naoleynik@edu.hse.ru>
2023-05-18 16:46:25 +0300

    tests_checking | commit 1

--- tasks/tests_checking/tests_checking.cpp
+++ tasks/tests_checking/tests_checking.cpp
@@ -1,6 +1,16 @@
+#include <deque>
 #include "tests_checking.h"
 
 std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student_actions,
                                        const std::vector<size_t>& queries) {
-    return {};
+    std::deque<std::string> order;
+    std::vector<std::string> students;
+    for (size_t i = 0; i < student_actions.size(); ++i) {
+        student_actions[i].side == Side::Top ? order.push_front(student_actions[i].name)
+                                             : order.push_back(student_actions[i].name);
+    }
+    for (size_t i = 0; i < queries.size(); ++i) {
+        students.push_back(order[queries[i] - 1]);
+    }
+    return students;
 }

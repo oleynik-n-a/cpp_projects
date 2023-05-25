5ea940568dee1c6f811ef09e4825f18cdc2536db
Nikita Oleynik <naoleynik@edu.hse.ru>
2023-05-25 03:57:20 +0300

    word2vec | commit 1

--- tasks/word2vec/word2vec.cpp
+++ tasks/word2vec/word2vec.cpp
@@ -4,5 +4,19 @@
 
 std::vector<std::string> FindClosestWords(const std::vector<std::string>& words,
                                           const std::vector<std::vector<int>>& vectors) {
-    return {};
+    std::vector<std::string> output;
+    int max = INT32_MIN;
+    for (int i = 1; i < static_cast<int>(vectors.size()); ++i) {
+        int scalar = vectors[0][0] * vectors[i][0] + vectors[0][1] * vectors[i][1] + vectors[0][2] * vectors[i][2];
+        if (max < scalar) {
+            max = scalar;
+        }
+    }
+    for (int i = 1; i < static_cast<int>(vectors.size()); ++i) {
+        int scalar = vectors[0][0] * vectors[i][0] + vectors[0][1] * vectors[i][1] + vectors[0][2] * vectors[i][2];
+        if (max == scalar) {
+            output.push_back(words[i]);
+        }
+    }
+    return output;
 }

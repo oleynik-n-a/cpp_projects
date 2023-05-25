fe12b6cc9fd27bcb5d2827a93705f1025e89627e
Nikita Oleynik <naoleynik@edu.hse.ru>
2023-05-25 22:43:23 +0300

    word2vec | thread resolving

--- tasks/word2vec/word2vec.cpp
+++ tasks/word2vec/word2vec.cpp
@@ -2,18 +2,26 @@
 
 #include <vector>
 
+int Scalar(std::vector<int> vector1, std::vector<int> vector2) {
+    int scalar = 0;
+    for (int i = 0; i < static_cast<int>(vector1.size()); ++i) {
+        scalar += vector1[i] * vector2[i];
+    }
+    return scalar;
+}
+
 std::vector<std::string> FindClosestWords(const std::vector<std::string>& words,
                                           const std::vector<std::vector<int>>& vectors) {
     std::vector<std::string> output;
-    int max = INT32_MIN;
+    int max = Scalar(vectors[0], vectors[1]);
     for (int i = 1; i < static_cast<int>(vectors.size()); ++i) {
-        int scalar = vectors[0][0] * vectors[i][0] + vectors[0][1] * vectors[i][1] + vectors[0][2] * vectors[i][2];
+        int scalar = Scalar(vectors[0], vectors[i]);
         if (max < scalar) {
             max = scalar;
         }
     }
     for (int i = 1; i < static_cast<int>(vectors.size()); ++i) {
-        int scalar = vectors[0][0] * vectors[i][0] + vectors[0][1] * vectors[i][1] + vectors[0][2] * vectors[i][2];
+        int scalar = Scalar(vectors[0], vectors[i]);
         if (max == scalar) {
             output.push_back(words[i]);
         }

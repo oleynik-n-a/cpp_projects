b46ebca9013b42577aacb48c2752c164b627e6c0
Nikita Oleynik <naoleynik@edu.hse.ru>
2023-05-27 20:49:08 +0300

    scorer | alg fix 2

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -5,14 +5,14 @@ ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
     sort(events_copy.begin(), events_copy.end(), [](Event event1, Event event2) { return event1.time < event2.time; });
     std::map<StudentName, std::set<TaskName>> score_table;
     for (size_t i = 0; i < events_copy.size(); ++i) {
-        if ((events_copy[i].event_type == EventType::CheckSuccess ||
-             events_copy[i].event_type == EventType::MergeRequestClosed) &&
-            events_copy[i].time <= score_time) {
+        if (events_copy[i].time > score_time) {
+            break;
+        }
+        if (events_copy[i].event_type == EventType::CheckSuccess ||
+            events_copy[i].event_type == EventType::MergeRequestClosed) {
             score_table[events_copy[i].student_name].insert(events_copy[i].task_name);
         } else {
-            if (score_table.contains(events_copy[i].student_name)) {
-                score_table[events_copy[i].student_name].erase(events_copy[i].task_name);
-            }
+            score_table[events_copy[i].student_name].erase(events_copy[i].task_name);
         }
     }
     return score_table;

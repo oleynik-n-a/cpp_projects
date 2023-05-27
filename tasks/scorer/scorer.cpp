307f1a32f0c110736dfadbef50d093a29ea78ec6
Nikita Oleynik <naoleynik@edu.hse.ru>
2023-05-27 16:58:08 +0300

    scorer | code style fix

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -3,8 +3,9 @@
 ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
     std::map<StudentName, std::set<TaskName>> score_table;
     for (size_t i = 0; i < events.size(); ++i) {
-        if ((events[i].event_type == EventType::CheckSuccess || events[i].event_type == EventType::MergeRequestClosed)
-            && events[i].time <= score_time) {
+        if ((events[i].event_type == EventType::CheckSuccess ||
+             events[i].event_type == EventType::MergeRequestClosed) &&
+            events[i].time <= score_time) {
             score_table[events[i].student_name].insert(events[i].task_name);
         } else {
             if (score_table.contains(events[i].student_name)) {

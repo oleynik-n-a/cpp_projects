17465d6419c75d05707eab8466a9451ce622ca8e
Nikita Oleynik <naoleynik@edu.hse.ru>
2023-05-27 16:55:43 +0300

    scorer | commit 1

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -1,5 +1,16 @@
 #include "scorer.h"
 
 ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
-    return {}; // test
+    std::map<StudentName, std::set<TaskName>> score_table;
+    for (size_t i = 0; i < events.size(); ++i) {
+        if ((events[i].event_type == EventType::CheckSuccess || events[i].event_type == EventType::MergeRequestClosed)
+            && events[i].time <= score_time) {
+            score_table[events[i].student_name].insert(events[i].task_name);
+        } else {
+            if (score_table.contains(events[i].student_name)) {
+                score_table[events[i].student_name].erase(events[i].task_name);
+            }
+        }
+    }
+    return score_table;
 }

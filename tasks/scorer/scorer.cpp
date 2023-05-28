f659e02ab0f4f92e47110f22eb792355d8ad4e21
Nikita Oleynik <naoleynik@edu.hse.ru>
2023-05-28 22:18:25 +0300

    scorer | added pointer sorting

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -3,19 +3,23 @@
 #include <algorithm>
 
 ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
-    Events events_copy = events;
-    sort(events_copy.begin(), events_copy.end(), [](Event event1, Event event2) { return event1.time < event2.time; });
+    std::vector<const Event*> events_copy(events.size());
+    for (size_t i = 0; i < events.size(); ++i) {
+        events_copy[i] = &events[i];
+    }
+    sort(events_copy.begin(), events_copy.end(), [](const Event* event1, const Event* event2) {
+                                                                   return event1->time < event2->time; });
     std::map<StudentName, std::set<TaskName>> score_table;
     for (size_t i = 0; i < events_copy.size(); ++i) {
-        if (events_copy[i].time > score_time) {
+        if (events_copy[i]->time > score_time) {
             break;
         }
-        if (events_copy[i].event_type == EventType::CheckSuccess ||
-            events_copy[i].event_type == EventType::MergeRequestClosed) {
-            score_table[events_copy[i].student_name].insert(events_copy[i].task_name);
+        if (events_copy[i]->event_type == EventType::CheckSuccess ||
+            events_copy[i]->event_type == EventType::MergeRequestClosed) {
+            score_table[events_copy[i]->student_name].insert(events_copy[i]->task_name);
         } else {
-            if (score_table.contains(events_copy[i].student_name)) {
-                score_table[events_copy[i].student_name].erase(events_copy[i].task_name);
+            if (score_table.contains(events_copy[i]->student_name)) {
+                score_table[events_copy[i]->student_name].erase(events_copy[i]->task_name);
             }
         }
     }

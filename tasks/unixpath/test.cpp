e31d01569a151189a19118a8793a856cfbd4cb13
Nikita Oleynik <naoleynik@edu.hse.ru>
2023-05-28 20:25:57 +0300

    unixpath | alg fix 3

--- tasks/unixpath/test.cpp
+++ tasks/unixpath/test.cpp
@@ -5,9 +5,14 @@ TEST_CASE("NormalizePath") {
     REQUIRE("/" == NormalizePath("/", "."));
     REQUIRE("/home/user2" == NormalizePath("/home/user1", "../user2"));
 
-    REQUIRE("/a/b/c/f" == NormalizePath("/a/b/c/d", "../c/f"));
+    REQUIRE("/" == NormalizePath("/", ".."));
     REQUIRE("/tmp" == NormalizePath("/home", "../../tmp"));
 
     REQUIRE("/a" == NormalizePath("/", "../../a/"));
     REQUIRE("/b" == NormalizePath("/", ".././/././/./../b/././././././"));
+
+    REQUIRE("/a/a" == NormalizePath("/a", ".//././a/"));
+    REQUIRE("/a/b/c/f" == NormalizePath("/a/b/c/d", "../c/f"));
+    REQUIRE("/path" == NormalizePath("/path", "/"));
+    REQUIRE("/abc" == NormalizePath("/abc/cde", ".."));
 }

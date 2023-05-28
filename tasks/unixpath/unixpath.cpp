8a95993b8980f101ad84005496d7bfb83b7cea9a
Nikita Oleynik <naoleynik@edu.hse.ru>
2023-05-28 21:36:01 +0300

    unixpath | code style fix 5

--- tasks/unixpath/unixpath.cpp
+++ tasks/unixpath/unixpath.cpp
@@ -28,12 +28,11 @@ std::string NormalizePath(std::string_view current_working_dir, std::string_view
             while (current + j < path.size() && !(path[current + j] == '.' || path[current + j] == '/')) {
                 ++j;
             }
-            if (result.substr(result.find_last_of('/') + 1, result.size() -
-                                                            result.find_last_of('/') - 1) != path.substr(current, j)) {
+            if (result.substr(result.find_last_of('/') + 1, result.size() - result.find_last_of('/') - 1) !=
+                path.substr(current, j)) {
                 result = result + (result.ends_with('/') ? "" : "/") + std::string(path.substr(current, j));
                 current += j - 1;
             }
-
         }
     }
     if (result.empty()) {

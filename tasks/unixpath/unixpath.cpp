a14249a7a2955ba784701503d07a53c73bd3969f
Nikita Oleynik <naoleynik@edu.hse.ru>
2023-05-28 14:08:42 +0300

    unixpath | commit 1

--- tasks/unixpath/unixpath.cpp
+++ tasks/unixpath/unixpath.cpp
@@ -1,5 +1,26 @@
 #include "unixpath.h"
 
 std::string NormalizePath(std::string_view current_working_dir, std::string_view path) {
-    return "";
+    for (size_t current = 0; current < path.size() - 1; ++current) {
+        if (path[current] == '/') {
+            continue;
+        } else if (path[current] == '.') {
+            if (path[current + 1] == '.') {
+                current_working_dir = current_working_dir.substr(0,current_working_dir.find_last_of('/'));
+                ++current;
+            }
+        } else {
+            int j = 0;
+            while (current + j < path.size() && !(path[current + j] == '.' || path[current + j] == '/')) {
+                ++j;
+            }
+            current_working_dir = std::string_view(std::string(current_working_dir) + "/" +
+                    std::string(path.substr(current, j)));
+            current += j - 1;
+        };
+    }
+    if (current_working_dir.empty()) {
+        return "/";
+    }
+    return std::string(current_working_dir);
 }

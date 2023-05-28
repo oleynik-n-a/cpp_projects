76fa2d2880433ff64643f4286f4e29d6a0d93fc5
Nikita Oleynik <naoleynik@edu.hse.ru>
2023-05-28 19:08:43 +0300

    unixpath | alg fix 2

--- tasks/unixpath/test.cpp
+++ tasks/unixpath/test.cpp
@@ -5,7 +5,7 @@ TEST_CASE("NormalizePath") {
     REQUIRE("/" == NormalizePath("/", "."));
     REQUIRE("/home/user2" == NormalizePath("/home/user1", "../user2"));
 
-    REQUIRE("/" == NormalizePath("/", ".."));
+    REQUIRE("/a/b/c/f" == NormalizePath("/a/b/c/d", "../c/f"));
     REQUIRE("/tmp" == NormalizePath("/home", "../../tmp"));
 
     REQUIRE("/a" == NormalizePath("/", "../../a/"));
--- tasks/unixpath/unixpath.cpp
+++ tasks/unixpath/unixpath.cpp
@@ -1,15 +1,16 @@
 #include "unixpath.h"
 
 std::string NormalizePath(std::string_view current_working_dir, std::string_view path) {
+    std::string result = std::string(current_working_dir);
     if (path.empty()) {
-        return std::string(current_working_dir);
+        return result;
     }
-    for (size_t current = 0; current < path.size() - 1; ++current) {
+    for (size_t current = 0; current < path.size(); ++current) {
         if (path[current] == '/') {
             continue;
         } else if (path[current] == '.') {
-            if (path[current + 1] == '.') {
-                current_working_dir = current_working_dir.substr(0, current_working_dir.find_last_of('/'));
+            if (current + 1 < path.size() && path[current + 1] == '.') {
+                result = result.substr(0, result.find_last_of('/'));
                 ++current;
             }
         } else {
@@ -17,14 +18,15 @@ std::string NormalizePath(std::string_view current_working_dir, std::string_view
             while (current + j < path.size() && !(path[current + j] == '.' || path[current + j] == '/')) {
                 ++j;
             }
-            current_working_dir =
-                std::string_view(std::string(current_working_dir) + (current_working_dir.ends_with('/') ? "" : "/") +
-                                 std::string(path.substr(current, j)));
-            current += j - 1;
+            if (result.substr(result.find_last_of('/') + 1, result.size() -
+                    result.find_last_of('/') - 1) != path.substr(current, j)) {
+                result = result + (result.ends_with('/') ? "" : "/") + std::string(path.substr(current, j));
+                current += j - 1;
+            }
         }
     }
-    if (current_working_dir.empty()) {
+    if (result.empty()) {
         return "/";
     }
-    return std::string(current_working_dir);
+    return result;
 }

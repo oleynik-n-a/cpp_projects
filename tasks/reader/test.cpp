86589b828d4685f0bcb48f0bab1ffcd4a86a2e88
Mikhail Fedorov <misha17061995@yandex.ru>
2023-05-30 12:16:06 +0300

    Drop random generator

--- tasks/reader/test.cpp
+++ tasks/reader/test.cpp
@@ -1,5 +1,4 @@
 #include "catch.hpp"
-#include <util.h>
 
 #include <iostream>
 #include <cassert>
@@ -11,7 +10,7 @@ std::unique_ptr<Reader> MakeStringReader(const std::string& data) {
     return std::unique_ptr<StringReader>(new StringReader(data));
 }
 
-TEST_CASE("StringReader") {
+TEST_CASE("Public StringReader") {
     REQUIRE(ReadAll(MakeStringReader("").get()).empty());
     REQUIRE("a" == ReadAll(MakeStringReader("a").get()));
 
@@ -19,7 +18,7 @@ TEST_CASE("StringReader") {
     REQUIRE(big_string == ReadAll(MakeStringReader(big_string).get()));
 }
 
-TEST_CASE("LimitReader") {
+TEST_CASE("Public LimitReader") {
     LimitReader l1(MakeStringReader(""), 0);
     REQUIRE(ReadAll(&l1).empty());
 
@@ -28,12 +27,6 @@ TEST_CASE("LimitReader") {
 
     LimitReader l3(MakeStringReader("ab"), 1);
     REQUIRE("a" == ReadAll(&l3));
-
-    RandomGenerator rnd(42);
-    std::string big_string = rnd.GenString(500, 'A', 'A' + 9);
-
-    LimitReader l4(MakeStringReader(big_string), 123);
-    REQUIRE(big_string.substr(0, 123) == ReadAll(&l4));
 }
 
 TeeReader MakeTee(std::vector<std::string> chunks) {
@@ -44,7 +37,7 @@ TeeReader MakeTee(std::vector<std::string> chunks) {
     return TeeReader(std::move(readers));
 }
 
-TEST_CASE("TeeReader") {
+TEST_CASE("Public TeeReader") {
     TeeReader t1({});
     REQUIRE(ReadAll(&t1).empty());
 
@@ -62,15 +55,9 @@ TEST_CASE("TeeReader") {
 
     TeeReader t6 = MakeTee({"abc", "", "", "", "", "def", "", ""});
     REQUIRE("abcdef" == ReadAll(&t6));
-
-    RandomGenerator rnd(43);
-    std::string big_string = rnd.GenString(500, 'A', 'A' + 9);
-
-    TeeReader t7 = MakeTee({big_string, big_string, big_string});
-    REQUIRE(big_string + big_string + big_string == ReadAll(&t7));
 }
 
-TEST_CASE("HexReader") {
+TEST_CASE("Public HexReader") {
     HexDecodingReader h1(MakeStringReader(""));
     REQUIRE(ReadAll(&h1).empty());
 

414f0cc2038844ae1a1070266d1d1599d986057f
Oleynik Nikita <naoleynik@edu.hse.ru>
2023-06-01 17:36:02 +0300

    dedup | commit 1

--- tasks/dedup/dedup.h
+++ tasks/dedup/dedup.h
@@ -7,7 +7,28 @@
 using std::string;
 
 template <class T>
-std::vector<std::unique_ptr<T>> Duplicate(const std::vector<std::shared_ptr<T>>& items);
+std::vector<std::unique_ptr<T>> Duplicate(const std::vector<std::shared_ptr<T>>& items) {
+    std::vector<std::unique_ptr<T>> result;
+    for (size_t i = 0; i < items.size(); ++i) {
+        result.emplace_back(std::make_unique<T>(*items[i]));
+    }
+    return result;
+}
 
 template <class T>
-std::vector<std::shared_ptr<T>> DeDuplicate(const std::vector<std::unique_ptr<T>>& items);
+std::vector<std::shared_ptr<T>> DeDuplicate(const std::vector<std::unique_ptr<T>>& items) {
+    struct Comrapison {
+        bool operator() (const std::shared_ptr<T>& l_ptr, const std::shared_ptr<T>& r_ptr) const{
+            return *l_ptr < *r_ptr;
+        }
+    };
+
+    std::set<std::shared_ptr<T>, Comrapison> pool;
+    std::vector<std::shared_ptr<T>> result;
+    for (size_t i = 0; i < items.size(); ++i) {
+        auto copy = std::make_shared<T>(*items[i]);
+        auto item = *pool.insert(copy).first;
+        result.push_back(item);
+    }
+    return result;
+}

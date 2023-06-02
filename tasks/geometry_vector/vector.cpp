a3a683f1a786979b0c01bee1e1d1f20b400bd9b7
Nikita Oleynik <naoleynik@edu.hse.ru>
2023-06-02 18:22:09 +0300

    geometry_vector | code style fix 2

--- tasks/geometry_vector/vector.cpp
+++ tasks/geometry_vector/vector.cpp
@@ -3,97 +3,79 @@
 namespace geometry {
 Vector::Vector() = default;
 
-Vector::Vector(int64_t x, int64_t y)
-{
+Vector::Vector(int64_t x, int64_t y) {
     x_coord_ = x;
     y_coord_ = y;
 }
 
-int64_t Vector::GetX() const
-{
+int64_t Vector::GetX() const {
     return x_coord_;
 }
 
-int64_t Vector::GetY() const
-{
+int64_t Vector::GetY() const {
     return y_coord_;
 }
 
-Vector Vector::operator+(const Vector& other) const
-{
+Vector Vector::operator+(const Vector& other) const {
     return Vector(x_coord_ + other.x_coord_, y_coord_ + other.y_coord_);
 }
 
-Vector Vector::operator-(const Vector& other) const
-{
+Vector Vector::operator-(const Vector& other) const {
     return Vector(x_coord_ - other.x_coord_, y_coord_ - other.y_coord_);
 }
 
-Vector Vector::operator+() const
-{
+Vector Vector::operator+() const {
     return *this;
 }
 
-Vector Vector::operator-() const
-{
+Vector Vector::operator-() const {
     return Vector(-x_coord_, -y_coord_);
 }
 
-Vector Vector::operator/(int64_t number) const
-{
-    if (number != 0)
-    {
+Vector Vector::operator/(int64_t number) const {
+    if (number != 0) {
         return Vector(x_coord_ / number, y_coord_ / number);
     }
     return Vector();
 }
 
-Vector Vector::operator*(int64_t number) const
-{
+Vector Vector::operator*(int64_t number) const {
     return Vector(x_coord_ * number, y_coord_ * number);
 }
 
-Vector& Vector::operator+=(const Vector& other)
-{
+Vector& Vector::operator+=(const Vector& other) {
     *this = *this + other;
     return *this;
 }
 
-Vector& Vector::operator-=(const Vector& other)
-{
+Vector& Vector::operator-=(const Vector& other) {
     *this = *this - other;
     return *this;
 }
 
-Vector& Vector::operator*=(int64_t number)
-{
+Vector& Vector::operator*=(int64_t number) {
     *this = *this * number;
     return *this;
 }
 
-Vector& Vector::operator/=(int64_t number)
-{
+Vector& Vector::operator/=(int64_t number) {
     *this = *this / number;
     return *this;
 }
 
-bool Vector::operator==(const geometry::Vector& other) const
-{
+bool Vector::operator==(const geometry::Vector& other) const {
     return x_coord_ == other.x_coord_ && y_coord_ == other.y_coord_;
 }
 
-int64_t ScalarMult(const Vector& first, const Vector& second)
-{
+int64_t ScalarMult(const Vector& first, const Vector& second) {
     return first.x_coord_ * second.x_coord_ + first.x_coord_ * second.y_coord_;
 }
 
-int64_t VectorMult(const Vector& first, const Vector& second)
-{
+int64_t VectorMult(const Vector& first, const Vector& second) {
     return first.x_coord_ * second.x_coord_ - first.x_coord_ * second.y_coord_;
 }
 
-double Length(Vector vector)
-{
+double Length(Vector vector) {
     return sqrt(static_cast<double>(vector.x_coord_ * vector.x_coord_ + vector.y_coord_ * vector.y_coord_));
 }
 } // namespace geometry

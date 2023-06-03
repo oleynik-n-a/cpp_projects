7788dde9e992757d5859905267d54a9957ec4276
Nikita Oleynik <naoleynik@edu.hse.ru>
2023-06-03 17:06:40 +0300

    geometry_segment | CrossesSegment fix 2

--- tasks/geometry_segment/segment.cpp
+++ tasks/geometry_segment/segment.cpp
@@ -66,7 +66,7 @@ bool Segment::ContainsPoint(const Point& point) const {
 }
 
 bool Segment::CrossesSegment(const Segment& segment) const {
-    if (ScalarMult(Vector(GetEnd().GetX() - GetStart().GetX(), GetEnd().GetY() - GetStart().GetY()),
+    if (VectorMult(Vector(GetEnd().GetX() - GetStart().GetX(), GetEnd().GetY() - GetStart().GetY()),
                    Vector(segment.GetEnd().GetX() - segment.GetStart().GetX(),
                           segment.GetEnd().GetY() - segment.GetStart().GetY())) == 0 &&
         !segment.ContainsPoint(GetStart()) && !segment.ContainsPoint(GetEnd())) {

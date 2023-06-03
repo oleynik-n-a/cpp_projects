704feb6c70312e6872cd828e705ca44bd15eec07
Nikita Oleynik <naoleynik@edu.hse.ru>
2023-06-03 17:14:25 +0300

    geometry_segment | CrossesSegment fix 3

--- tasks/geometry_segment/segment.cpp
+++ tasks/geometry_segment/segment.cpp
@@ -66,10 +66,12 @@ bool Segment::ContainsPoint(const Point& point) const {
 }
 
 bool Segment::CrossesSegment(const Segment& segment) const {
-    if (VectorMult(Vector(GetEnd().GetX() - GetStart().GetX(), GetEnd().GetY() - GetStart().GetY()),
-                   Vector(segment.GetEnd().GetX() - segment.GetStart().GetX(),
-                          segment.GetEnd().GetY() - segment.GetStart().GetY())) == 0 &&
-        !segment.ContainsPoint(GetStart()) && !segment.ContainsPoint(GetEnd())) {
+    if (Line(Point(GetStart().GetX(), GetStart().GetY()), Point(GetEnd().GetX(), GetEnd().GetY()))
+            .ContainsPoint(segment.GetStart()) &&
+        Line(Point(GetStart().GetX(), GetStart().GetY()), Point(GetEnd().GetX(), GetEnd().GetY()))
+            .ContainsPoint(segment.GetEnd()) &&
+        !segment.ContainsPoint(GetStart()) && !segment.ContainsPoint(GetEnd()) && !ContainsPoint(segment.GetStart()) &&
+        !ContainsPoint(segment.GetEnd())) {
         return false;
     }
     return (Line(Point(GetStart().GetX(), GetStart().GetY()), Point(GetEnd().GetX(), GetEnd().GetY()))

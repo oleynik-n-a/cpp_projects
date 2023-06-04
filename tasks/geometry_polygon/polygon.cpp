5a5e374babd2867c7ec10ebe3dc800d2afaef5f7
Nikita Oleynik <naoleynik@edu.hse.ru>
2023-06-04 13:54:34 +0300

    geometry_polygon | commit 1

---
+++ tasks/geometry_polygon/polygon.cpp
@@ -0,0 +1,53 @@
+#include "segment.h"
+
+#include <cmath>
+#include "point.h"
+#include "polygon.h"
+#include "vector.h"
+
+namespace geometry {
+Polygon::Polygon() = default;
+
+Polygon::Polygon(std::vector<Point> points) {
+    points_ = points;
+    num_points_ = points.size();
+}
+
+Polygon& Polygon::Move(const Vector& vector) {
+    for (size_t i = 0; i < num_points_; ++i) {
+        points_[i].SetX(points_[i].GetX() + vector.GetX());
+        points_[i].SetY(points_[i].GetY() + vector.GetY());
+    }
+    return *this;
+}
+
+bool Polygon::ContainsPoint(const geometry::Point& point) const {
+    for (size_t i = 0; i < num_points_; ++i) {
+        if (Segment(points_[i], points_[(i + 1) % num_points_]).ContainsPoint(point)) {
+            return true;
+        }
+    }
+    double angles_sum = 0;
+    const double eps = 0.0000000001;
+    for (size_t i = 0; i < num_points_; ++i) {
+        Vector v1 = points_[i] - point;
+        Vector v2 = points_[(i + 1) % num_points_] - point;
+        Vector v3 = points_[i] - points_[(i + 1) % num_points_];
+        angles_sum += acos((pow(Length(v1), 2) + pow(Length(v2), 2) - pow(Length(v3), 2)) / (2 * Length(v1) * Length(v2)));
+    }
+    return angles_sum >= 2 * std::numbers::pi - eps && angles_sum <= 2 * std::numbers::pi + eps;
+}
+
+bool Polygon::CrossesSegment(const geometry::Segment& segment) const {
+    for (size_t i = 0; i < num_points_; ++i) {
+        if (segment.CrossesSegment(Segment(points_[i], points_[(i + 1) % num_points_]))) {
+            return true;
+        }
+    }
+    return false;
+}
+
+Polygon* Polygon::Clone() const {
+    return new Polygon(points_);
+}
+}  // namespace geometry

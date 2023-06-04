6094619b3d35d4d834a2f1006eda1d58796f6006
Nikita Oleynik <naoleynik@edu.hse.ru>
2023-06-04 14:19:15 +0300

    geometry_polygon | alg fix 2

--- tasks/geometry_polygon/polygon.cpp
+++ tasks/geometry_polygon/polygon.cpp
@@ -22,7 +22,7 @@ Polygon& Polygon::Move(const Vector& vector) {
     return *this;
 }
 
-bool Polygon::ContainsPoint(const geometry::Point& point) const {
+bool Polygon::ContainsPoint(const Point& point) const {
     for (size_t i = 0; i < num_points_; ++i) {
         if (Segment(points_[i], points_[(i + 1) % num_points_]).ContainsPoint(point)) {
             return true;
@@ -31,16 +31,19 @@ bool Polygon::ContainsPoint(const geometry::Point& point) const {
     double angles_sum = 0;
     const double eps = 0.00000001;
     for (size_t i = 0; i < num_points_; ++i) {
-        Vector v1 = points_[i] - point;
-        Vector v2 = points_[(i + 1) % num_points_] - point;
-        Vector v3 = points_[i] - points_[(i + 1) % num_points_];
-        angles_sum +=
-            acos((pow(Length(v1), 2) + pow(Length(v2), 2) - pow(Length(v3), 2)) / (2 * Length(v1) * Length(v2)));
+        if (!(points_[i].GetX() == points_[(i + 1) % num_points_].GetX() &&
+              points_[i].GetY() == points_[(i + 1) % num_points_].GetY())) {
+            Vector v1 = points_[i] - point;
+            Vector v2 = points_[(i + 1) % num_points_] - point;
+            Vector v3 = points_[i] - points_[(i + 1) % num_points_];
+            angles_sum +=
+                    acos((pow(Length(v1), 2) + pow(Length(v2), 2) - pow(Length(v3), 2)) / (2 * Length(v1) * Length(v2)));
+        }
     }
     return angles_sum >= 2 * std::numbers::pi - eps && angles_sum <= 2 * std::numbers::pi + eps;
 }
 
-bool Polygon::CrossesSegment(const geometry::Segment& segment) const {
+bool Polygon::CrossesSegment(const Segment& segment) const {
     for (size_t i = 0; i < num_points_; ++i) {
         if (segment.CrossesSegment(Segment(points_[i], points_[(i + 1) % num_points_]))) {
             return true;

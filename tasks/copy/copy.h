99b236ab88409d2c29cabc8d6d16e42c40136809
Nikita Oleynik <naoleynik@edu.hse.ru>
2023-06-05 18:59:04 +0300

    copy | commit 1

--- tasks/copy/copy.h
+++ tasks/copy/copy.h
@@ -1,7 +1,19 @@
 #pragma once
 
 template <typename T, typename U>
-U Copy(T src_begin, T src_end, U dest);
+U Copy(T src_begin, T src_end, U dest) {
+    U result = dest;
+    for (T iterator = src_begin; iterator < src_end;) {
+        *result++ = *iterator++;
+    }
+    return result;
+}
 
 template <typename T, typename U>
-U CopyBackward(T src_begin, T src_end, U dest);
+U CopyBackward(T src_begin, T src_end, U dest) {
+    U result = dest;
+    for (T iterator = src_end; iterator > src_begin;) {
+        *(--result) = *(--iterator);
+    }
+    return result;
+}

74ad457e4edabed9f86f324feeacdbeacd9e0405
Nikita Oleynik <naoleynik@edu.hse.ru>
2023-06-05 21:46:22 +0300

    rotate | commit 1

--- tasks/rotate/rotate.h
+++ tasks/rotate/rotate.h
@@ -1,7 +1,17 @@
 #pragma once
 
-template <typename Iterator>
-void Reverse(Iterator begin, Iterator end);
+#include <queue>
 
 template <typename Iterator>
-void Rotate(Iterator begin, Iterator mid, Iterator end);
+void Reverse(Iterator begin, Iterator end) {
+    for (Iterator iterator = begin; iterator < begin + (end - begin) / 2; ++iterator) {
+        std::swap(*iterator, *(begin + (end - iterator - 1)));
+    }
+}
+
+template <typename Iterator>
+void Rotate(Iterator begin, Iterator mid, Iterator end) {
+    Reverse(begin, mid);
+    Reverse(mid, end);
+    Reverse(begin, end);
+}

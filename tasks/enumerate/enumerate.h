bd1c686ed5639bcc3fdea63bdb7c148fbd68aaa3
Nikita Oleynik <naoleynik@edu.hse.ru>
2023-06-17 14:26:22 +0300

    enumerate | commit 1

--- tasks/enumerate/enumerate.h
+++ tasks/enumerate/enumerate.h
@@ -1,6 +1,8 @@
 #pragma once
 
-template <class Iterator>
+#include <iostream>
+
+template<class Iterator>
 class IteratorRange {
 public:
     IteratorRange(Iterator begin, Iterator end) : begin_(begin), end_(end) {
@@ -17,3 +19,33 @@ public:
 private:
     Iterator begin_, end_;
 };
+
+template<class Iterator>
+class RangeIterator {
+private:
+    size_t id_;
+    Iterator cur_;
+
+public:
+    explicit RangeIterator(size_t id, Iterator cur) : id_(id), cur_(cur) {
+    }
+
+    RangeIterator& operator++() {
+        ++id_;
+        ++cur_;
+        return *this;
+    }
+
+    std::pair<size_t, decltype(*cur_)> operator*() const {
+        return {id_, *cur_};
+    }
+
+    bool operator!=(const RangeIterator& rhs) const {
+        return cur_ != rhs.cur_;
+    }
+};
+
+template<class Container>
+auto Enumerate(Container& c) {
+    return IteratorRange(RangeIterator(0, c.begin()), RangeIterator(std::distance(c.begin(), c.end()), c.end()));
+}
